d65a37e2bfef0356cbb8a136adae1da073988926
AhmedOsman101 <trollingosman101@outlook.com>
2024-12-03 20:24:43 +0200

    Updated Files

---
+++ logs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+log_info() {
+  echo -e "\033[32m[INFO]: $1\033[0m"
+}
+
+log_warning() {
+  echo -e "\033[33m[WARNING]: $1\033[0m"
+}
+
+log_error() {
+  echo -e "\033[31m[ERROR]: $1\033[0m" >&2
+  exit 1
+}
