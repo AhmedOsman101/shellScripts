2144f02a4b0d9526e5d74b6aa1b0fa360f64a2e8
AhmedOsman101 <trollingosman101@outlook.com>
2025-01-12 15:30:28 +0200

    Updated Files 2025-01-12@03:30PM

--- logs.sh
+++ logs.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 log_info() {
-	echo -e "\033[32m[INFO]: $1\033[0m"
+	echo -e "\033[34m[INFO]: $1\033[0m"
+}
+
+log_success() {
+	echo -e "\033[32m[SUCCESS]: $1\033[0m"
 }
 
 log_warning() {
