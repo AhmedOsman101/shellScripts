ac32ef8bc2cafe52ed9e8f27fdf1186a82df84de
AhmedOsman101 <trollingosman101@outlook.com>
2025-02-19 23:28:29 +0200

    feat: add dotfiles selection script
    - Uses eza and gum for directory selection
    - Prints the selected config directory

---
+++ dotfiles.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $# -eq 0 ]]; then 
+  directory=$(eza "${TUCKR_DIR}" \
+  --color=always \
+  --only-dirs \
+  --icons=always \
+  --long \
+  --no-time \
+  --no-user \
+  --sort name \
+  --no-permissions \
+  --no-filesize | gum filter \
+  --placeholder="Search..." \
+  --header="Choose a config directory" \
+  --fuzzy | cut -d " " -f 2-
+)
+else
+  directory=$1
+fi
+
+echo "${TUCKR_DIR}/${directory}/.config"
