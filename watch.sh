e209295595f3bfebb7d24199a24c1d1814541f62
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-14 03:23:18 +0200

    feat: add watch script using watchexec
    - Added a `watch.sh` script to automatically run commands on file changes.
    - Uses `watchexec` to monitor file changes based on specified extensions.
    - Prompts the user for file extensions and the command to execute if not provided as arguments.
    - Includes error handling for missing `watchexec` installation.
    - Configured with a 1-second delay before running the command and a 5-second debounce to prevent excessive executions.

---
+++ watch.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+trap 'exit 1' SIGUSR1 # Terminate script on SIGUSR1
+
+# Check if 'watchexec' is installed
+if ! command -v watchexec &>/dev/null; then
+  echo "'watchexec' is not installed. Please install it to proceed." >&2
+  exit 1
+fi
+
+# Get the extensions string
+# Prompt user for extensions if not provided as arguments
+if [[ -z "$1" ]]; then
+  extensions_str=$(gum input --header="Enter file extensions (space-separated, e.g., ts js md):")
+else
+  extensions_str="$1"
+  shift
+fi
+
+# Convert extensions string to array
+extensionsArray=("${extensions_str}")
+
+# Build a comma-separated string of extensions
+extensions=$(
+  IFS=,
+  echo "${extensionsArray[*]}"
+)
+
+# Prompt user for the command to run if not provided
+if [[ -z "$2" ]]; then
+  command_to_run=$(gum input --header="Enter the command to execute on changes:")
+  # read -r command_to_run
+else
+  shift
+  command_to_run="$*"
+fi
+
+# Run watchexec with the provided command
+watchexec -c --timings \
+  --delay-run 1s \
+  --debounce 5s \
+  --exts "${extensions}" \
+  -- "${command_to_run}"
