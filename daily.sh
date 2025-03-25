d6db2fc48afbd877fd140cd6f5ba44cc7171fda0
AhmedOsman101 <trollingosman101@outlook.com>
2025-03-25 21:18:13 +0200

    Updated Files 2025-03-25@09:18PM

--- daily.sh
+++ daily.sh
@@ -2,11 +2,20 @@
 
 DOTFILES="${HOME}/dotfiles"
 
+chassis=$(hostnamectl chassis)
+
+if [[ "${chassis}" == "laptop" ]]; then
+  device="laptop"
+else
+  device="pc"
+fi
+
 # ---- paru packages ---- #
-paru -Qqe >"${DOTFILES}/pc_packages.txt"
+paru -Qqe >"${DOTFILES}/${device}_packages.txt"
 
 # ---- vscode extenstions---- #
-get-ext "${DOTFILES}/pc_extensions.json" -o
+get-ext "${DOTFILES}/${device}_extensions.json" -o
 
 # ---- pnpm ---- #
-pnpm-ls >"${DOTFILES}/pnpm_global_packages.txt"
+pnpm-ls >>"${DOTFILES}/pnpm_global_packages.txt"
+no-dups "${DOTFILES}/pnpm_global_packages.txt"
