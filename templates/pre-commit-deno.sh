537fc50e1a56418e011a53bb4d35c5ba70ab9787
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-08-05 21:09:44 +0300

    feat: add biome pre-commit hooks for js
    - add pre-commit hooks for bun, deno, npm, pnpm, yarn
    - format staged files using biome
    - add formatted files to stage

---
+++ templates/pre-commit-deno.sh
@@ -0,0 +1,6 @@
+# shellcheck disable=SC2046,SC2148
+# Formats only the staged files
+deno run -A npm:@biomejs/biome check --fix --no-errors-on-unmatched $(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g' | paste -sd' ' -)
+
+# Adds staged files again (to include the new formatter changes)
+git add $(git diff --cached --name-only --diff-filter=ACMR)
