471052c6825017143e745b2a4c12ed1322bedfc6
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-08-20 09:47:06 +0300

    refactor: improve pre-commit script file handling
    - use xargs for file processing
    - change to git root for script execution

--- templates/pre-commit-deno.sh
+++ templates/pre-commit-deno.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env bash
 # shellcheck disable=SC2046,SC2148
+
+gitRoot="$(git rev-parse --show-toplevel)"
+
+cd "${gitRoot}" || exit 1
+
 # Formats only the staged files
-deno run -A npm:@biomejs/biome check --fix --no-errors-on-unmatched $(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g' | paste -sd' ' -)
+git diff --cached --name-only --diff-filter=ACMR | xargs -d '\n' deno run -A npm:@biomejs/biome check --fix --no-errors-on-unmatched
 
 # Adds staged files again (to include the new formatter changes)
-git add $(git diff --cached --name-only --diff-filter=ACMR)
+git diff --cached --name-only --diff-filter=ACMR | xargs -d '\n' git add
+
+exit 0
