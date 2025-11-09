d18fb7997106c7c20f995adb4c662770209b9dd7
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-11-09 22:22:35 +0200

    feat: process files or strip emojis from text
    - allows processing files or text
    - adds deno shebang
    - improves error handling

--- typescript/emoji-strip/emoji-strip.ts
+++ typescript/emoji-strip/emoji-strip.ts
@@ -1,5 +1,9 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write
+
 import emojiRegex from "emoji-regex";
 import { wrapAsync } from "lib-result";
+
+const encoder = new TextEncoder();
 
 // Function to print to stderr in red
 function printRed(message: string): never {
@@ -7,35 +11,55 @@ function printRed(message: string): never {
   Deno.exit(1);
 }
 
-async function processFile(path: string) {
-  const regex = emojiRegex();
-
-  const statResult = await wrapAsync(() => Deno.stat(path));
-  if (statResult.isOk() && statResult.ok.isFile) {
-    const text = await wrapAsync(() => Deno.readTextFile(path));
-    if (text.isError()) {
-      printRed(`An error occured while reading file ${path}: ${text.error}`);
-    }
-    const cleaned = text.ok.replaceAll(regex, "");
-
-    if (cleaned !== text.ok) {
-      const writeResult = await wrapAsync(() =>
-        Deno.writeTextFile(path, cleaned, { create: false })
-      );
-      if (writeResult.isError()) {
-        printRed(
-          `An error occured while writing to file ${path}: ${writeResult.error}`
-        );
-      } else console.log(`Processed file '${path}' successfully!`);
-    } else console.log("Nothing to do!");
-  } else console.warn(`${path} is not a file`);
+async function print(...args: string[]) {
+  await Deno.stdout.write(encoder.encode(args.join(" ")));
 }
 
-function main(argv: string[], argc: number) {
-  if (argc === 0) printRed("Usage: emoji-strip <file1> [file2...]");
-  argv.forEach(async path => await processFile(path));
+const stripEmojies = (str: string) => str.replaceAll(emojiRegex(), "");
+
+async function processFile(path: string) {
+  const text = await wrapAsync(() => Deno.readTextFile(path));
+  if (text.isError()) {
+    printRed(`An error occured while reading file ${path}: ${text.error}`);
+  }
+
+  const cleaned = stripEmojies(text.ok);
+  if (cleaned === text.ok) {
+    await print(`Nothing to clean in '${path}'\n`);
+    return;
+  }
+
+  const writeResult = await wrapAsync(() =>
+    Deno.writeTextFile(path, cleaned, { create: false })
+  );
+  if (writeResult.isError()) {
+    printRed(
+      `An error occured while writing to file ${path}: ${writeResult.error}`
+    );
+  } else await print(`Processed file '${path}' successfully!\n`);
+}
+
+async function processArg(arg: string) {
+  const statResult = await wrapAsync(() => Deno.stat(arg));
+
+  if (statResult.isOk() && statResult.ok.isFile) {
+    await print("\n");
+    await processFile(arg);
+  } else {
+    // Treat argument as raw text input
+    await print(`${stripEmojies(arg)} `);
+  }
+}
+
+async function main(argv: string[], argc: number) {
+  if (argc === 0) {
+    printRed("Usage: emoji-strip <file1|text> [more files or text...]");
+  }
+
+  for (const arg of argv) await processArg(arg);
+  await print("\n");
 }
 
 if (import.meta.main) {
-  main(Deno.args, Deno.args.length);
+  await main(Deno.args, Deno.args.length);
 }
