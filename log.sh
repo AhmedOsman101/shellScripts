8af154651a27bb7e60e5fc573c3fb6e0f2106fbd
Ahmad Othman <ahmad.ali.othman@outlook.com>
2026-04-02 22:12:13 +0200

    Updated Files 2026-04-02@10:12PM

--- log.sh
+++ log.sh
@@ -20,14 +20,9 @@
 set -eo pipefail
 trap 'exit 1' SIGUSR1
 
-# eval "$(include "lib/cmdarg.sh")"
 eval "$(include "lib/helpers.sh")"
 eval "$(include "check-deps")"
 checkDeps "$0"
-
-# --- cmdarg setup --- #
-# cmdarg_info "header" "$(get-desc "$0")"
-# cmdarg_parse "$@"
 # ---  Main script logic --- #
 
 # --- Define color mapping --- #
